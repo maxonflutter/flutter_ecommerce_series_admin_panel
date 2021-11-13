0f3362ae19464cafe4e27910e6a690ef218e7a2b
Massimo Del Pezzo <massimo.dpezzo@gmail.com>
2021-11-13 17:46:48 +0100

    Product Screen

--- lib/screens/new_product_screen.dart
+++ lib/screens/new_product_screen.dart
@@ -18,9 +18,57 @@ class NewProductScreen extends StatelessWidget {
         padding: const EdgeInsets.all(10.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [],
+          children: [
+            SizedBox(
+              height: 100,
+              child: Card(
+                margin: EdgeInsets.zero,
+                color: Colors.black,
+                child: Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Get.to(() => const NewProductScreen());
+                      },
+                      icon: const Icon(
+                        Icons.add_circle,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      'Add an Image',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Product Information',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            _buildTextFormField('Product ID'),
+            _buildTextFormField('Product Name'),
+            _buildTextFormField('Product Description'),
+            _buildTextFormField('Product Category'),
+            const SizedBox(height: 10),
+          ],
         ),
       ),
+    );
+  }
+
+  TextFormField _buildTextFormField(String hintText) {
+    return TextFormField(
+      decoration: InputDecoration(hintText: hintText),
     );
   }
 }
